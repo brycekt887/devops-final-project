3f3a42af8cf015332f4dc7a0288b46b0dffea05e
brycekt887 <brycekt@byui.edu>
2026-04-04 14:39:11 -0600

    made the html look better and added css with cool welcome pic

--- app.js
+++ app.js
@@ -7,11 +7,44 @@ app.get("/", (req, res) => {
     <html>
       <head>
         <title>DevOps Final Project</title>
+        <style>
+          body {
+          font-family: Arial;
+          background-color: #e6e6e6; /* light grey background */
+          margin: 0;
+          padding: 40px;
+          }
+
+          h1 {
+            color: #333;
+          }
+
+          img {
+            width: 300px;
+            margin-top: 20px;
+            border-radius: 10px;
+          }
+
+          .box {
+            background: white;
+            padding: 20px;
+            margin: auto;
+            width: 400px;
+            border-radius: 10px;
+            box-shadow: 0px 4px 12px rgba(0,0,0,0.1);
+            text-align: center;
+          }
+        </style>
       </head>
+
       <body>
-        <h1>DevOps Final Project</h1>
-        <p>The website is running successfully.</p>
-        <p>Try visiting <a href="/health">/health</a> to check app status.</p>
+        <div class="box">
+          <h1>DevOps Final Project</h1>
+          <p>The website is running successfully.</p>
+          <p><a href="/health">Check Health Status</a></p>
+
+          <img src="https://images.pexels.com/photos/33802176/pexels-photo-33802176.jpeg" />
+        </div>
       </body>
     </html>
   `);
