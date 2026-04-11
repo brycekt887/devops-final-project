7ad055f9329da2f6b3c12528ca893b93c1dcb6c6
Benjamin Loughmiller <bloughmiller@byui.edu>
2026-04-11 10:58:32 -0600

    Edited Terraform files. Set release.yml to run whether main succeeds or not

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
-  value = aws_instance.web.public_ip
+  description = "The public IP for the EC2 instance"
+  value       = aws_instance.web.public_ip 
+}
+
+output "app_url" {
+  description = "The URL to access the application"
+  value       = "http://${aws_instance.web.public_ip}:3000"
 }
--- iac/variables.tf
+++ iac/variables.tf
@@ -1,22 +1,11 @@
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
-}
-
-variable "ami_id" {
-  description = "Amazon Linux 2 AMI ID"
-  type        = string
-  default     = "ami-0c2ab3f1443d7d2d7"
-}
-
-variable "docker_image" {
-  description = "Docker Hub image to run"
-  type        = string
 }
